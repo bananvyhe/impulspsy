fef2179c21859e4e9451f503f7f295753c5eb739
bananvyhe <loadonden@yahoo.com>
2016-10-15 17:02:16 +0500

    add devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get '/' => 'home#index'
+  
+
+  devise_for :users
+root to: "home#index"
+
+ get '/' => 'home#index'
 
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
