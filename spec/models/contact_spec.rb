603c0e781f4837c2902d7dd77954a1d882c666e3
bananvyhe <loadonden@yahoo.com>
2016-10-21 20:20:36 +0500

    add message model validation test

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -3,10 +3,6 @@ require "rails_helper"
 describe Contact do 
 
 	it { should validate_presence_of (:email) }
+	it { should validate_presence_of (:message) }
 end
 
-
-#describe "something" do
-#	it "does something" do
-#	end
-#end
